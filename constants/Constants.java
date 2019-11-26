d1760c057e90524373da2ff0f7454cb19bb5f5f8
mirub <55086342+mirub@users.noreply.github.com>
2019-11-27 00:29:37 +0200

    Create Constants.java

---
+++ constants/Constants.java
@@ -0,0 +1,91 @@
+package constants;
+
+public class Constants {
+    // Pyromancer constants:
+
+    public static final int INITIAL_PYRO_HP = 500;
+    public static final int PYRO_HP_LEVEL_BONUS = 50;
+
+    public static final float FIREBLAST_DAMAGE = 350f;
+    public static final float FIREBLAST_DAMAGE_BONUS = 50f;
+    public static final float FIREBLAST_ROGUE_PERCENT = 0.8f;
+    public static final float FIREBLAST_KNIGHT_PERCENT = 1.2f;
+    public static final float FIREBLAST_PYRO_PERCENT = 0.9f;
+    public static final float FIREBLAST_WIZARD_PERCENT = 1.05f;
+
+    public static final float IGNITE_DAMAGE_FIRST = 150f;
+    public static final float IGNITE_DAMAGE_FIRST_LEVEL_BONUS = 20f;
+    public static final float IGNITE_DAMAGE = 50f;
+    public static final float IGNITE_DAMAGE_LEVEL_BONUS = 30f;
+    public static final float IGNITE_ROGUE_PERCENT = 0.8f;
+    public static final float IGNITE_KNIGHT_PERCENT = 1.2f;
+    public static final float IGNITE_PYRO_PERCENT = 0.9f;
+    public static final float IGNITE_WIZARD_PERCENT = 1.05f;
+
+    public static final float PYRO_VOLCANIC_PERCENT = 1.25f;
+
+    // Knight constants:
+
+    public static final int INITIAL_KNIGHT_HP = 900;
+    public static final int KNIGHT_HP_LEVEL_BONUS = 80;
+
+    public static final float EXECUTE_DAMAGE = 200f;
+    public static final float EXECUTE_DAMAGE_BONUS = 30f;
+    public static final float EXECUTE_ROGUE_PERCENT = 1.15f;
+    public static final float EXECUTE_KNIGHT_PERCENT = 1f;
+    public static final float EXECUTE_PYRO_PERCENT = 1.1f;
+    public static final float EXECUTE_WIZARD_PERCENT = 0.8f;
+
+    public static final float SLAM_DAMAGE = 100f;
+    public static final float SLAM_DAMAGE_LEVEL_BONUS = 40f;
+    public static final float SLAM_ROGUE_PERCENT = 0.8f;
+    public static final float SLAM_KNIGHT_PERCENT = 1.2f;
+    public static final float SLAM_PYRO_PERCENT = 0.9f;
+    public static final float SLAM_WIZARD_PERCENT = 1.05f;
+
+    public static final float KNIGHT_LAND_PERCENT = 1.15f;
+
+    // Wizard constants:
+
+    public static final int INITIAL_WIZARD_HP = 400;
+    public static final int WIZARD_HP_LEVEL_BONUS = 30;
+
+    public static final float DRAIN_DAMAGE_PERCENT = 0.2f;
+    public static final float DRAIN_DAMAGE_PERCENT_BONUS = 0.05f;
+    public static final float DRAIN_ROGUE_PERCENT = 0.8f;
+    public static final float DRAIN_KNIGHT_PERCENT = 1.2f;
+    public static final float DRAIN_PYRO_PERCENT = 0.9f;
+    public static final float DRAIN_WIZARD_PERCENT = 1.05f;
+
+    public static final float DEFLECT_DAMAGE_PERCENT = 0.35f;
+    public static final float DEFLECT_DAMAGE_LEVEL_BONUS = 0.02f;
+    public static final float DEFLECT_ROGUE_PERCENT = 1.2f;
+    public static final float DEFLECT_KNIGHT_PERCENT = 1.4f;
+    public static final float DEFLECT_PYRO_PERCENT = 1.3f;
+    public static final float DEFLECT_WIZARD_PERCENT = 0f;
+
+    public static final float WIZARD_DESERT_PERCENT = 1.10f;
+
+    // Rogue constants:
+
+    public static final int INITIAL_ROGUE_HP = 600;
+    public static final int ROGUE_HP_LEVEL_BONUS = 40;
+
+    public static final float BACKSTAB_DAMAGE = 200f;
+    public static final float BACKSTAB_DAMAGE_LEVEL_BONUS = 20f;
+    public static final float BACKSTAB_ROGUE_PERCENT = 1.2f;
+    public static final float BACKSTAB_KNIGHT_PERCENT = 0.9f;
+    public static final float BACKSTAB_PYRO_PERCENT = 1.25f;
+    public static final float BACKSTAB_WIZARD_PERCENT = 1.25f;
+
+    public static final float PARALYSIS_DAMAGE_PERCENT = 0.9f;
+    public static final float PARALYSIS_DAMAGE_LEVEL_BONUS = 10f;
+    public static final float PARALYSIS_ROGUE_PERCENT = 0.9f;
+    public static final float PARALYSIS_KNIGHT_PERCENT = 0.8f;
+    public static final float PARALYSIS_PYRO_PERCENT = 1.2f;
+    public static final float PARALYSIS_WIZARD_PERCENT = 1.25f;
+
+    public static final float ROGUE_WOODS_PERCENT = 1.15f;
+
+
+}
