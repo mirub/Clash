d00956277271b0da9dde7d7093aa4f83502ea2f6
mirub <55086342+mirub@users.noreply.github.com>
2019-12-01 12:34:58 +0200

    Delete Fought.java

--- players/Fought.java
+++
@@ -1,7 +0,0 @@
-/* Banu Miruna Elena - 321CA - League of OOP - Stage 1 - 2019 */
-package player;
-
-/* The Visitor Pattern Visitable Interface*/
-public interface Fought {
-    void accept(Fighter v);
-}
