0d83ccd3a34e609c03b1877632285a5bf0513480
mirub <55086342+mirub@users.noreply.github.com>
2019-12-01 11:49:26 +0200

    Add files via upload

--- ground/BattleField.java
+++ ground/BattleField.java
@@ -1,20 +1,21 @@
+/* Banu Miruna Elena - 321CA - League of OOP - Stage 1 - 2019 */
 package ground;
 
 import java.util.List;
 
-public class BattleField {
-    private List<String> ground;
+public final class BattleField {
+    private final List<String> ground;
     private static BattleField instance = null;
 
 
-    public static BattleField getInstance(List<String> ground) {
+    public static BattleField getInstance(final List<String> ground) {
         if (instance == null) {
             instance = new BattleField(ground);
         }
         return instance;
     }
 
-    private BattleField(List<String> ground) {
+    private BattleField(final List<String> ground) {
         this.ground = ground;
     }
 
